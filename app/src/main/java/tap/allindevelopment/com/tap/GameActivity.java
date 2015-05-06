6262a7c5165e93b827e8d929c9f78a5a126a3d21
empoci_k <kevin.empociello@epitech.eu>
2015-05-06 16:01:11 +0200

    1.1 Replay work

--- app/src/main/java/tap/allindevelopment/com/tap/GameActivity.java
+++ app/src/main/java/tap/allindevelopment/com/tap/GameActivity.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.akexorcist.roundcornerprogressbar.RoundCornerProgressBar;
@@ -28,63 +30,101 @@ public class GameActivity extends Activity {
     TextView totalpoints;
     Typeface fontPoints;
     Integer   globalPoints;
+    Display display;
     CountDownTimer lol;
     int check_final = 0;
+    ImageView tapLogo;
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_game);
-
+            display = getWindowManager().getDefaultDisplay();
+            tapLogo = (ImageView) findViewById(R.id.logo);
             displayMap(0);
         }
 
         public void EndGame()
         {
-            final ImageView tapLogo = (ImageView) findViewById(R.id.logo);
             final ImageView image = (ImageView) findViewById(R.id.imageView);
             final ImageView image2 = (ImageView) findViewById(R.id.imageView2);
             final ImageView image3 = (ImageView) findViewById(R.id.imageView3);
             final ImageView image4 = (ImageView) findViewById(R.id.imageView4);
+            final ImageView replay = (ImageView) findViewById(R.id.replay);
+            final ImageView quit = (ImageView) findViewById(R.id.quit);
             TextView score = (TextView) findViewById(R.id.points);
-            RoundCornerProgressBar barProg = (RoundCornerProgressBar) findViewById(R.id.progress_1);
+/*            RoundCornerProgressBar barProg = (RoundCornerProgressBar) findViewById(R.id.progress_1);
             barProg.setProgress(0);
-            if (check_final == 1)
-                lol.onFinish();
-            barProg.setVisibility(View.INVISIBLE);
+            barProg.setVisibility(View.INVISIBLE);*/
+            check_final = 1;
             image.setVisibility(View.INVISIBLE);
             image2.setVisibility(View.INVISIBLE);
             image3.setVisibility(View.INVISIBLE);
             image4.setVisibility(View.INVISIBLE);
-            moveLogo(tapLogo);
+            moveLogo2(tapLogo);
             moveScore(score);
             score.setTextSize(40);
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    replay.setVisibility(View.VISIBLE);
+                    quit.setVisibility(View.INVISIBLE);
+                    checkRestart();
+                }
+            }, 700);
+        }
+
+        public void checkRestart() {
+            ImageView replay = (ImageView) findViewById(R.id.replay);
+            replay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    check_final = 0;
+                    moveLogo(tapLogo);
+                    displayMap(0);
+                }
+            });
         }
 
         private void moveLogo( View view )
         {
-            TranslateAnimation anim = new TranslateAnimation( 0, 0, 0, -600);
-            anim.setDuration(700);
+            ImageView letest = (ImageView) findViewById(R.id.logo);
+            TextView score = (TextView) findViewById(R.id.points);
+            final ImageView replay = (ImageView) findViewById(R.id.replay);
+            final ImageView quit = (ImageView) findViewById(R.id.quit);
+            replay.setVisibility(View.INVISIBLE);
+            quit.setVisibility(View.INVISIBLE);
+            score.setTextSize(20);
+            globalPoints = 0;
+            score.setText("Score: " + Integer.toString(globalPoints));
+            Display display = getWindowManager().getDefaultDisplay();
+            double height = display.getHeight()/2.1000 * -1;
+            TranslateAnimation anim = new TranslateAnimation( 0, 0, 0, 0);
+            anim.setDuration(1000);
             anim.setFillAfter(true);
-            view.startAnimation(anim);
+            letest.startAnimation(anim);
+            score.startAnimation(anim);
         }
 
     private void moveLogo2( View view )
     {
-        Display display = getWindowManager().getDefaultDisplay();
-        double height = (display.getHeight()) * -1;
+        double height = (display.getHeight()/2.100) * -1;
         TranslateAnimation anim = new TranslateAnimation( 0, 0, 0, (float)height);
         anim.setDuration(700);
         anim.setFillAfter(true);
         view.startAnimation(anim);
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)tapLogo.getLayoutParams();
+        view.setLayoutParams(params);
     }
 
     private void moveScore( View view )
     {
-        TranslateAnimation anim = new TranslateAnimation( 0, 0, 0, 500);
+        double height = (display.getHeight()/2.100);
+        TranslateAnimation anim = new TranslateAnimation(0, 0, 0, (float)height);
         anim.setDuration(700);
         anim.setFillAfter( true );
         view.startAnimation(anim);
+
     }
 
 
@@ -92,7 +132,7 @@ public class GameActivity extends Activity {
     {
         TranslateAnimation anim = new TranslateAnimation( 0, 0, 0, -500);
         anim.setDuration(700);
-        anim.setFillAfter( true );
+        anim.setFillAfter(true);
         view.startAnimation(anim);
     }
 
@@ -116,13 +156,14 @@ public class GameActivity extends Activity {
                 image4.setImageAlpha(255);
             }
         } else {
-            check_final = 1;
-            EndGame();
+            lol.onFinish();
         }
     }
 
     public void displayMap(int pos)
         {
+            RoundCornerProgressBar barProg = (RoundCornerProgressBar) findViewById(R.id.progress_1);
+            barProg.setVisibility(View.VISIBLE);
             ImageView square = (ImageView) findViewById(R.id.square);
             square.setVisibility(View.VISIBLE);
             square.setOnClickListener(new View.OnClickListener(){
@@ -142,6 +183,7 @@ public class GameActivity extends Activity {
                     image2 = (ImageView) findViewById(R.id.imageView2);
                     image3 = (ImageView) findViewById(R.id.imageView3);
                     image4 = (ImageView) findViewById(R.id.imageView4);
+
                     image.setVisibility(View.VISIBLE);
                     image2.setVisibility(View.VISIBLE);
                     image3.setVisibility(View.VISIBLE);
@@ -217,18 +259,21 @@ public class GameActivity extends Activity {
 
                     int test = 100;
                     lol = new CountDownTimer(20000, 100) {
-
                         public void onTick(long millisUntilFinished) {
-                            RoundCornerProgressBar progress2 = (RoundCornerProgressBar) findViewById(R.id.progress_1);
-                            double progress1 = (millisUntilFinished  / 1000.000) *  5.000;
-                            progress2.setProgress((float)progress1);
+                                RoundCornerProgressBar progress2 = (RoundCornerProgressBar) findViewById(R.id.progress_1);
+                                double progress1 = (millisUntilFinished / 1000.000) * 5.000;
+                                progress2.setProgress((float) progress1);
                         }
 
                         public void onFinish() {
                             RoundCornerProgressBar progress2 = (RoundCornerProgressBar) findViewById(R.id.progress_1);
-                            progress2.setProgress(0);
-                            if (check_final == 0)
+                            progress2.setProgress(100);
+                            progress2.setVisibility(View.INVISIBLE);
+                            if (check_final == 0) {
+                                lol.cancel();
                                 EndGame();
+                            }
+                            check_final = 1;
                             Log.i("temps :", "done!");
                         }
                     }.start();
